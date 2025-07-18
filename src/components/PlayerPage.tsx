d6f10d9ff7cda764211751f16f0e63d3df3ebbf5
Anthony <gutierrezanthony250@gmail.com>
2025-07-18 10:58:27 -0400

    feat: :sparkle: funcionalidad de reproductor

--- src/components/PlayerPage.tsx
+++ src/components/PlayerPage.tsx
@@ -1,28 +1,41 @@
-import React from 'react';
-import './PlayerPage.css'; 
+import React, { useEffect } from 'react';
+import './PlayerPage.css';
+import { $currentSong } from '../store';
+import type { Song } from '../types/song';
 
 export default function PlayerPage() {
-    return (
-        <div className="player-container">
-  <div className="cover">
-    <img src="/cover.jpg" alt="Song Cover" />
-  </div>
-  <div className="song-info">
-    <h2 className="title">Song Title</h2>
-    <p className="artist">Artist Name</p>
-  </div>
-  <div className="progress-bar">
-    <span className="current-time">0:00</span>
-    <div className="bar">
-      <div className="progress"></div>
+
+  const [song, setSong] = React.useState<null | Song>(null)
+
+  React.useEffect(function () {
+    $currentSong.subscribe(function (state) {
+      setSong(state)
+
+    })
+  }, [])
+
+  return (
+    <div className="player-container">
+      <audio src={song?.audio.url} controls autoPlay></audio>
+      <div className="cover">
+        <img src={song?.image.url} alt="Song Cover" />
+      </div>
+      <div className="song-info">
+        <h2 className="title">{song?.title}</h2>
+        <p className="artist">{song?.author}</p>
+      </div>
+      <div className="progress-bar">
+        <span className="current-time">0:00</span>
+        <div className="bar">
+          <div className="progress"></div>
+        </div>
+        <span className="duration">3:45</span>
+      </div>
+      <div className="controls">
+        <button className="prev">&#9664;</button>
+        <button className="play">&#9654;</button>
+        <button className="next">&#9654;&#9654;</button>
+      </div>
     </div>
-    <span className="duration">3:45</span>
-  </div>
-  <div className="controls">
-    <button className="prev">&#9664;</button>
-    <button className="play">&#9654;</button>
-    <button className="next">&#9654;&#9654;</button>
-  </div>
-</div>
-    );
+  );
 }   
